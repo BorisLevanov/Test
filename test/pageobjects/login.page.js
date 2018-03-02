2566ef598acd2fc9f441848cc25337f6d7b42114
borislevanov <bobhlop@gmail.com>
2018-03-02 08:50:16 -0800

    development: login tests stable

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -5,10 +5,14 @@ var LoginPage = Object.create(Page, {
     /**
      * define elements
      */
+    // Elements
     username:           { get: function () { return browser.element('[placeholder="Username"]'); } },
     password:           { get: function () { return browser.element('[placeholder="Password"]'); } },
     submitButton:       { get: function () { return browser.element('[type="submit"]'); } },
+
+    // Page hooks
     pageBody:           { get: function () { return browser.element('.container'); } },
+    validationMessage:  { get: function () { return browser.element('[role="alert"]'); } },
 
     /**
      * define or overwrite page methods
--- test/specs/MDN-010.1.js
+++ test/specs/MDN-010.1.js
@@ -1,26 +1,58 @@
 // login.spec.js
 var expect = require('chai').expect;
 var LoginPage = require('../pageobjects/login.page');
+var TasksPage = require('../pageobjects/tasks.page');
 
-describe('login form', function () {
-    it('should deny access with wrong creds', function () {
+
+describe('A user should be able to log in.', function () {
+    it('Given a user logs in with incorrect credentials, a validation message should appear.', function () {
         LoginPage.open();
         LoginPage.username.setValue('foo');
         LoginPage.password.setValue('bar');
         LoginPage.submit();
 
-        // Wait for the validion
-        browser.waitForVisible('[role="alert"]');
-
+        // Wait for the validation to appear
+        LoginPage.validationMessage.waitForVisible(10000);
         expect(LoginPage.pageBody.getText()).to.contain('Oops! Could not authenticate user');
     });
 
-    it('should allow access with correct creds', function () {
+    it('Given a regular user logs in with correct credentials, they should be navigated to the Tasks page.', function () {
         LoginPage.open();
-        LoginPage.username.setValue('admin');        
+        LoginPage.username.clearElement();
+        LoginPage.username.setValue('user');
+        LoginPage.password.clearElement();
         LoginPage.password.setValue('123456');
         LoginPage.submit();
 
-        expect(LoginPage.pageBody.getText()).to.contain('You logged into a secure area!');
+        TasksPage.signOutButton.waitForVisible(10000);
+        expect(LoginPage.pageBody.getText()).to.contain('My Tasks');
+        TasksPage.signOutButton.click();
+
+    });
+
+    it('Given a manager user logs in with correct credentials, they should be navigated to the Tasks page.', function () {
+        LoginPage.open();
+        LoginPage.username.clearElement();
+        LoginPage.username.setValue('manager');
+        LoginPage.password.clearElement();
+        LoginPage.password.setValue('123456');
+        LoginPage.submit();
+
+        TasksPage.signOutButton.waitForVisible(10000);
+        expect(LoginPage.pageBody.getText()).to.contain('My Tasks');
+        TasksPage.signOutButton.click();
+    });
+
+    it('Given an admin user logs in with correct credentials, they should be navigated to the Tasks page.', function () {
+        LoginPage.open();
+        LoginPage.username.clearElement();
+        LoginPage.username.setValue('user');
+        LoginPage.password.clearElement();
+        LoginPage.password.setValue('123456');
+        LoginPage.submit();
+
+        TasksPage.signOutButton.waitForVisible(10000);       
+        expect(LoginPage.pageBody.getText()).to.contain('My Tasks');
+        TasksPage.signOutButton.click();
     });
 });
